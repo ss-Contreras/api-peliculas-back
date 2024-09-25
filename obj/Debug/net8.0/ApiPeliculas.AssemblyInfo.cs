b135131d2254ada1767c6d369cbafaec2fb77ea4
Sergio Contreras <sergiocontreras2003ss@gmail.com>
2024-09-25 10:53:50 -0500

    Actualizacion File Imagen

--- obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d066a0dde53e30040b3b3a38d72105ba6da601")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
